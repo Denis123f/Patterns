d7ea947f3dee3394521ca06e4f470dc4687ebfbc
Den Pl <del.9990@mail.ru>
2025-03-23 10:54:03 +0500

    Testing

---
+++ Strategy.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+
+template <typename T>
+class Search_Straregy{
+
+    public:
+    virtual T& search(std::vector<T> values) const = 0;
+
+};
+
+template <typename T>
+class Search_Max : public Search_Straregy<T>{
+
+public:
+
+    T& search(std::vector<T> values) const override;
+
+};
+
+template <typename T>
+class Search_Min : public Search_Straregy<T>{
+
+public:
+    T& search(std::vector<T> values) const override;
+
+};
+
+template <typename T>
+class Average_Value : public Search_Straregy<T>{
+
+public:
+    T& search(std::vector<T> values) const override;
+};
+
+template <typename T>
+class Working_with_Search{
+    Search_Straregy<T>* strategy;
+
+    void set_Strategy(Search_Straregy<T>& strategy){
+        this->strategy = strategy;
+    }
+
+    T& Doing_Search(const std::vector<T>& qwerty){
+        return strategy->search(qwerty);
+    }
+};
+int main(){
+    std::vector<int> ab = {1, 2, 3};
+    Working_with_Search<int>* set;
+}
